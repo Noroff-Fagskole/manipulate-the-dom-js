b719555b67f1e54229d2a59605122cf41f9b72d6
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-02-07 10:35:49 +0100

    text and color added to notification

--- exercise_1/js/script.js
+++ exercise_1/js/script.js
@@ -7,6 +7,13 @@ There is a notification banner on the site
 
 1. add a friendly message to the notification "notification-content"
 2. add a class to make the banner green, use this class: has-background-primary from: https://bulma.io/documentation/modifiers/color-helpers/
-3. you will need to create an event listener and a function to do the above work
 
 */
+
+const notification = document.querySelector('.notification-content');
+notification.innerHTML += 'Hello there!';
+
+notification.classList.add('has-background-primary');
+
+
+
